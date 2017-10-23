24b2725b1b17054969536ab259d35166b7f07fad
Nazar <nazarko1805@gmail.com>
2017-10-23 12:25:49 +0300

    [^] Comments

--- LABA3/src/com/lab3/Main.java
+++ LABA3/src/com/lab3/Main.java
@@ -5,6 +5,11 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
+/**
+ * Reading file and
+ * making arrays for
+ * integers
+ */
 public class Main {
     public int a =0,b=0,n=0;
     public void read() throws IOException {
@@ -26,32 +31,72 @@ public class Main {
         a = data[2];
     }
 
+    /**
+     * Calculating minimal
+     * size of square
+     */
     public void calculate() throws IOException {
-        double storona_kv = 0;
-        storona_kv =(int) Math.sqrt(n * a * b);
-        double suma_storin_by_h = 0,
-                sum_storin_by_w = 0,
-                count_rectangles_by_h = 0,
-                count_recktagles_by_w = 0;
+        double storona_kv = 0; //init side size of square
+        storona_kv =(int) Math.sqrt(n * a * b); //starting minimal side size
+        double suma_storin_by_h = 0, // sum of rectangle side sizes by height
+                sum_storin_by_w = 0, // sum of rectangle side size by width
+                count_rectangles_by_h = 0, // counter to count rectangles by their height
+                count_recktagles_by_w = 0; // counter to count rectangles by their width
 
         while(true){
+
+            /**
+             * Count number of
+             *  rectangles by
+             *  their heigth
+             *  */
             while (storona_kv - suma_storin_by_h > a) {
                 count_rectangles_by_h += 1;
                 suma_storin_by_h += a;
             }
+
+            /**
+             * Count number of
+             * rectangles by
+             * their width
+             */
             while (storona_kv - sum_storin_by_w > b) {
                 count_recktagles_by_w += 1;
                 sum_storin_by_w += b;
             }
+
+            /**
+             * Calculating available
+             * counted number of
+             *rectangles
+             */
             double rectangles_count = count_rectangles_by_h * count_recktagles_by_w;
+
+            /**
+             * Checking available counted
+             * rectangles with given
+             * in data file
+             */
             if (rectangles_count >= n) {
                 break;
             }
+
+            /**
+             * Adding that size that
+             * needs to put number
+             * of rectangles to get minimal
+             * size of square
+             */
             if (storona_kv - sum_storin_by_w < storona_kv - sum_storin_by_w) {
                 storona_kv += b - (storona_kv - sum_storin_by_w);
             } else {
                 storona_kv += a - (storona_kv - suma_storin_by_h);
             }
+
+            /**
+             * Write output
+             * to file
+             */
             BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
             writer.write(String.valueOf(storona_kv));
             writer.close();
