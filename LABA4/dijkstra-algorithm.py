2a93c13d715851da7830f94994d3a3a3d613f372
Zava <nazarko1805@gmail.com>
2017-11-20 10:34:33 +0200

    [^] New vers

--- LABA4/dijkstra-algorithm.py
+++ LABA4/dijkstra-algorithm.py
@@ -2,6 +2,7 @@ from collections import defaultdict, deque
 
 
 class Graph(object):
+    # makes the default value for all vertices an empty list
     def __init__(self):
         self.nodes = set()
         self.edges = defaultdict(list)
@@ -17,30 +18,39 @@ class Graph(object):
 
 
 def dijkstra(graph, initial):
+    # initializations
     visited = {initial: 0}
     path = {}
 
     nodes = set(graph.nodes)
 
     while nodes:
+        #while min node is none
         min_node = None
+        #go through nodes in nodes set
         for node in nodes:
             if node in visited:
                 if min_node is None:
+                    #set node that is visiterd first as minimal
                     min_node = node
+                    #if visited node is < than visited min node set it as min node
                 elif visited[node] < visited[min_node]:
                     min_node = node
+        #if min node is not found break
         if min_node is None:
             break
 
         nodes.remove(min_node)
+        #set curreng weight of node as min
         current_weight = visited[min_node]
 
         for edge in graph.edges[min_node]:
             try:
+                # node weigth + vertex to next node
                 weight = current_weight + graph.distances[(min_node, edge)]
             except:
                 continue
+                # if its not visited set them weight an min node
             if edge not in visited or weight < visited[edge]:
                 visited[edge] = weight
                 path[edge] = min_node
@@ -49,10 +59,12 @@ def dijkstra(graph, initial):
 
 
 def shortest_path(graph, origin, destination):
+    #set all path tha was found
     visited, paths = dijkstra(graph, origin)
     full_path = deque()
     _destination = paths[destination]
 
+    #while end point is not start point append destination to path
     while _destination != origin:
         full_path.appendleft(_destination)
         _destination = paths[_destination]
@@ -60,6 +72,7 @@ def shortest_path(graph, origin, destination):
     full_path.appendleft(origin)
     full_path.append(destination)
 
+    #return shortest way
     return visited[destination], list(full_path)
 
 if __name__ == '__main__':
